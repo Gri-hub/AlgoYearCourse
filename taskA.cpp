536f9d117ed83211c02d91a1f7ed73f6f8cdb4d4
Gri-hub <konoplev.grigory@gmail.com>
2022-10-31 22:13:52 +0300

    task A remake

--- taskA.cpp
+++ taskA.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 
 bool BinarySearch(const int* begin, const int* end, int target) {
-  long left_boundary = 0;
-  long right_boundary = end - begin;
+  size_t left_boundary = 0;
+  size_t right_boundary = end - begin;
   while (right_boundary - left_boundary > 1) {
-    long middle = (right_boundary + left_boundary) / 2;
+    size_t middle = (right_boundary + left_boundary) / 2;
     if (target >= *(begin + middle)) {
       left_boundary = middle;
     } else {
@@ -14,25 +14,39 @@ bool BinarySearch(const int* begin, const int* end, int target) {
   return (*(begin + left_boundary) == target);
 }
 
-int main() {
-  int array_length;
+void Read(size_t array_length, int array[]) {
   std::cin >> array_length;
-  int* array = new int[array_length];
-  for (int i = 0; i < array_length; i++) {
+  for (size_t i = 0; i < array_length; i++) {
     std::cin >> array[i];
   }
+}
 
+void Output(bool answer) {
+  if (answer) {
+    std::cout << "YES" << std::endl;
+  } else {
+    std::cout << "NO" << std::endl;
+  }
+}
+
+void ManageRequests(int array[]) {
   int number_of_calls;
   std::cin >> number_of_calls;
   for (int i = 0; i < number_of_calls; i++) {
     int begin, end, target;
     std::cin >> begin >> end >> target;
-    if (BinarySearch(array + begin, array + end, target)) {
-      std::cout << "YES" << std::endl;
-    } else {
-      std::cout << "NO" << std::endl;
-    }
+    Output(BinarySearch(array + begin, array + end, target));
   }
+}
+
+int main() {
+  const int kMaxInt = 1e5 + 1;
+  int* array = new int[kMaxInt];
+  size_t array_length = 0;
+
+  Read(array_length, array);
+  ManageRequests(array);
+
   delete[] array;
   return 0;
 }
