5d11f68b70981789ef3b6ea3ab8ef0de2dd42775
Gri-hub <konoplev.grigory@gmail.com>
2022-10-31 22:39:35 +0300

    Task B remake

--- taskH.cpp
+++ taskH.cpp
@@ -3,39 +3,48 @@
 #include <iostream>
 
 double FindAverageSound(int left_boundary, int right_boundary,
-                        const double array[], int array_size) {
+                        const double array[], size_t array_size) {
   double average_sound = array[right_boundary + 1] / array[left_boundary];
   double number_of_multipliers =
       (double)right_boundary - (double)left_boundary + (double)1;
   return std::pow(average_sound, (double)array_size / (number_of_multipliers));
 }
 
+void Read(size_t arr_size, double arr1[], double arr2[]) {
+  arr2[0] = 1;
+  for (size_t i = 0; i < arr_size; i++) {
+    std::cin >> arr1[i];
+    arr1[i] = std::pow(arr1[i], 1 / (double)arr_size);
+    arr2[i + 1] = arr1[i] * arr2[i];
+  }
+}
+
+void Out(double arr[], size_t arr_size) {
+  size_t requests;
+  std::cin >> requests;
+  int left_boundary;
+  int right_boundary;
+  for (size_t i = 0; i < requests; i++) {
+    std::cin >> left_boundary >> right_boundary;
+    std::cout << FindAverageSound(left_boundary, right_boundary, arr, arr_size);
+    std::cout << "\n";
+  }
+}
+
 int main() {
-  int number_of_creatures;
   std::cin >> std::setprecision(2);
+
+  size_t number_of_creatures;
   std::cin >> number_of_creatures;
   double* creature_sound = new double[number_of_creatures];
   double* array_of_products = new double[number_of_creatures + 1];
-  array_of_products[0] = 1;
-  for (int i = 0; i < number_of_creatures; i++) {
-    std::cin >> creature_sound[i];
-    creature_sound[i] =
-        std::pow(creature_sound[i], 1 / (double)number_of_creatures);
-    array_of_products[i + 1] = creature_sound[i] * array_of_products[i];
-  }
-  int number_of_requests;
-  std::cin >> number_of_requests;
-  int left_boundary;
-  int right_boundary;
+  Read(number_of_creatures, creature_sound, array_of_products);
+
   std::cout << std::fixed;
   const int kPrecision = 6;
   std::cout << std::setprecision(kPrecision);
-  for (int i = 0; i < number_of_requests; i++) {
-    std::cin >> left_boundary >> right_boundary;
-    std::cout << FindAverageSound(left_boundary, right_boundary,
-                                  array_of_products, number_of_creatures);
-    std::cout << "\n";
-  }
+  Out(array_of_products, number_of_creatures);
+
   delete[] creature_sound;
   delete[] array_of_products;
   return 0;
